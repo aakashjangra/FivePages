8a05248acfb80b0e2d1dfc30a9592c99c8a96873
aakashjangra <aakashjangra2002@gmail.com>
2025-03-17 17:47:07 +0530

    add route and controller for fetching latest chapters

--- fivepages-BE/src/routes/chapter.routes.js
+++ fivepages-BE/src/routes/chapter.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Chapter from '../models/chapter.models.js';
 import Novel from '../models/novel.models.js';
-import { createChapter, getAllChapters, getChapterByID } from '../controllers/chapter.controller.js';
+import { createChapter, getAllChapters, getChapterByID, getLatestChapters } from '../controllers/chapter.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -11,6 +11,9 @@ router.post('/add', verifyJWT, createChapter);
 
 // Get all chapters for a specific novel
 router.get('/novel/:novelId', getAllChapters);
+
+//get latest chapters
+router.get('/latest', getLatestChapters);
 
 // Get a single chapter by ID
 router.get('/:id', verifyJWT, getChapterByID);
