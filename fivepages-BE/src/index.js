a47d026835dfd888ff79329c546e7860c11cf325
aakashjangra <aakashjangra2002@gmail.com>
2025-03-17 12:47:42 +0530

    like, readlist routes and middlewares

--- fivepages-BE/src/index.js
+++ fivepages-BE/src/index.js
@@ -2,12 +2,17 @@ import express from 'express'
 import userRoutes from './routes/user.routes.js'
 import novelRoutes from './routes/novel.routes.js'
 import chapterRoutes from './routes/chapter.routes.js'
+import readlistRoutes from './routes/readlist.routes.js'
+import likeRoutes from './routes/like.routes.js'
+
+
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 //loading environment variables
 import 'dotenv/config'
 import connectDB from './config/db.js';
+import { verifyJWT } from './middlewares/auth.middleware.js'
 
 const app = express()
 
@@ -25,11 +30,16 @@ app.get('/api/v1/', (request, response) => {
 })
 
 //routes
-app.use('/api/v1/user', userRoutes)
+app.use('/api/v1/user', userRoutes);
 
 app.use('/api/v1/novels', novelRoutes);
 
 app.use('/api/v1/chapters', chapterRoutes);
+
+app.use('/api/v1/readlist', verifyJWT, readlistRoutes);
+
+app.use('/api/v1/like', verifyJWT, likeRoutes);
+
 
 const PORT = process.env.PORT || 3000
 
