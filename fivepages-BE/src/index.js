8fe3bed2654ab78d0672fdf38a7a7a6d56d3f5cc
aakashjangra <aakashjangra2002@gmail.com>
2025-03-17 17:35:02 +0530

    add comment functionality with routes, controller, and model

--- fivepages-BE/src/index.js
+++ fivepages-BE/src/index.js
@@ -4,7 +4,7 @@ import novelRoutes from './routes/novel.routes.js'
 import chapterRoutes from './routes/chapter.routes.js'
 import readlistRoutes from './routes/readlist.routes.js'
 import likeRoutes from './routes/like.routes.js'
-
+import commentRoutes from './routes/comment.routes.js'
 
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
@@ -40,6 +40,8 @@ app.use('/api/v1/chapters', chapterRoutes);
 app.use('/api/v1/readlist', verifyJWT, readlistRoutes);
 
 app.use('/api/v1/like', verifyJWT, likeRoutes);
+
+app.use('/api/v1/comments', verifyJWT, commentRoutes);
 
 
 const PORT = process.env.PORT || 3000
---
+++ fivepages-BE/src/models/comment.models.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+// comment can be either for a chapter or a novel
+// can't be both
+const commentSchema = new mongoose.Schema(
+  {
+    content: {type: String, required: true},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    novel: { type: mongoose.Schema.Types.ObjectId, ref: 'Novel'},
+    chapter: { type: mongoose.Schema.Types.ObjectId, ref: 'Chapter' },
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment;
