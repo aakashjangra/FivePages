81554bed23d9dbeca850887e2c0323f0a3b99402
aakashjangra <aakashjangra2002@gmail.com>
2025-03-17 12:47:05 +0530

    updated createNovel, getRecommendedNovels routes

--- fivepages-BE/src/routes/novel.routes.js
+++ fivepages-BE/src/routes/novel.routes.js
@@ -1,5 +1,6 @@
-import { createNovel, deleteNovel, getNovelByID, getNovels } from '../controllers/novel.controllers.js';
+import { createNovel, deleteNovel, getNovelByID, getNovels, getRecommendedNovels } from '../controllers/novel.controllers.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { upload } from '../middlewares/multer.middleware.js';
 import Novel from '../models/novel.models.js';
 import Router from 'express';
 
@@ -11,13 +12,15 @@ router.get('/', getNovels);
 // Get a single novel by ID
 router.get('/:id', getNovelByID);
 
+router.get('/:id/:count', getRecommendedNovels);
+
 // Create a new novel
-router.post('/', verifyJWT, createNovel);
+router.post('/', verifyJWT, upload.single('thumbnail'), createNovel);
 
 // Update a novel by ID
-router.put('/:id', getNovelByID);
+router.put('/:id', verifyJWT, getNovelByID);
 
 // Delete a novel by ID
-router.delete('/:id', deleteNovel);
+router.delete('/:id', verifyJWT, deleteNovel);
 
 export default router;
