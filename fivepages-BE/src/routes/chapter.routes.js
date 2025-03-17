328e70bf7f5d9ccb085c1b727f6ef7a85c50eff2
aakashjangra <aakashjangra2002@gmail.com>
2025-03-17 12:46:21 +0530

    auth added in chapter routes

--- fivepages-BE/src/routes/chapter.routes.js
+++ fivepages-BE/src/routes/chapter.routes.js
@@ -2,16 +2,17 @@ import express from 'express';
 import Chapter from '../models/chapter.models.js';
 import Novel from '../models/novel.models.js';
 import { createChapter, getAllChapters, getChapterByID } from '../controllers/chapter.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 // Create a new chapter
-router.post('/add', createChapter);
+router.post('/add', verifyJWT, createChapter);
 
 // Get all chapters for a specific novel
 router.get('/novel/:novelId', getAllChapters);
 
 // Get a single chapter by ID
-router.get('/:id', getChapterByID);
+router.get('/:id', verifyJWT, getChapterByID);
 
 export default router;
