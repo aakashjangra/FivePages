8e405f2b2909e81618464b1bffb9c22b7bc179cf
Anushka Kotiyal <anushkakotiyal12@gmail.com>
2025-03-25 23:42:14 +0530

    admin page creation

--- fivepages-BE/src/index.js
+++ fivepages-BE/src/index.js
@@ -24,7 +24,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:3000", // ✅ Set specific allowed origin
+  origin: "http://localhost:3003", // ✅ Set specific allowed origin
   credentials: true,
 }));
 
@@ -48,7 +48,7 @@ app.use('/api/v1/like', verifyJWT, likeRoutes);
 app.use('/api/v1/comments', verifyJWT, commentRoutes);
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`)
