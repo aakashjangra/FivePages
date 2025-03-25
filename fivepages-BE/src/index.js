459d735c8f98a9ce1599d55cc2ce4a96ef289ab3
OmYadav3 <omydv3@gmail.com>
2025-03-25 23:51:18 +0530

    change default port and unecessary file

--- fivepages-BE/src/index.js
+++ fivepages-BE/src/index.js
@@ -24,7 +24,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:3003", // ✅ Set specific allowed origin
+  origin: "http://localhost:3000", // ✅ Set specific allowed origin
   credentials: true,
 }));
 
