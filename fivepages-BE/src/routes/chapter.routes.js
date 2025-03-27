c9da46e37ca1affff7305be4c66532d1a56e76ce
aakashjangra <aakashjangra2002@gmail.com>
2025-03-27 22:03:02 +0530

    add morgan for logging and implement chapter update functionality with validation

--- fivepages-BE/src/routes/chapter.routes.js
+++ fivepages-BE/src/routes/chapter.routes.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import Chapter from '../models/chapter.models.js';
 import Novel from '../models/novel.models.js';
-import { createChapter, getAllChapters, getChapterByID, getLatestChapters } from '../controllers/chapter.controller.js';
+import { createChapter, getAllChapters, getChapterByID, getLatestChapters, updateChapter } from '../controllers/chapter.controller.js';
 import { verifyAdmin, verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 // Create a new chapter
 router.post('/add', verifyJWT, verifyAdmin, createChapter);
+
+//update chapter 
+router.patch('/:id', verifyJWT, verifyAdmin, updateChapter);
 
 // Get all chapters for a specific novel
 router.get('/novel/:novelId', getAllChapters);
